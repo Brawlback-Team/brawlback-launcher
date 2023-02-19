e3f39c7fe5dfb07f5f875f75f32e0b52c62ad3b1
Bradley Fargo <blewfargs@gmail.com>
2023-02-19 11:36:38 -0600

    commenting out some slippiTheme code, causes the typescript to become full of warnings

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,17 @@
 import "../src/renderer/styles/styles.scss"
 
-import { slippiTheme } from "../src/renderer/styles/theme";
+//import { slippiTheme } from "../src/renderer/styles/theme";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import React from 'react';
 import { addDecorator } from "@storybook/react";
 
+/*
 addDecorator((story) => {
   return (
     <MuiThemeProvider theme={slippiTheme}>{story()}</MuiThemeProvider>
   );
 });
-
+*/
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
