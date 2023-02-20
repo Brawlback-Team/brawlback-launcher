9ad14e408e336c397194209185cd3835400fda38
Bradley Fargo <blewfargs@gmail.com>
2023-02-20 00:09:40 -0600

    Revert "compare iso files with MD5 hash and Brawl versions"
    This reverts commit a9a0068351f8c63df2c5b0e135d1215ec3dc6ab2.

--- src/main/setup.ts
+++ src/main/setup.ts
@@ -27,7 +27,7 @@ import {
 } from "./ipc";
 import { fetchNewsFeedData } from "./newsFeed";
 import { getAssetPath, readLastLines } from "./util";
-import { _verifyIsoSHA1, verifyIsoMD5 } from "./verifyIso";
+import { verifyIso } from "./verifyIso";
 
 const log = electronLog.scope("main/listeners");
 const isDevelopment = process.env.NODE_ENV !== "production";
@@ -80,7 +80,7 @@ export default function setupMainIpc() {
     }
 
     try {
-      const result = await verifyIsoMD5(path);
+      const result = await verifyIso(path);
       return { path, valid: result };
     } catch (err) {
       return { path, valid: IsoValidity.INVALID };
