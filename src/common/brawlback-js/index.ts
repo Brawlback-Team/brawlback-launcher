7a3acd7d2c033e763b7227d37c8b55eb7b5a9672
Bradley Fargo <blewfargs@gmail.com>
2023-02-21 19:18:08 -0600

    well might as well add the rest of the slippi-js/console --no-verify to bypass linter

---
+++ src/common/brawlback-js/index.ts
@@ -0,0 +1,5 @@
+// taken directly from https://github.com/project-slippi/slippi-js/blob/master/src/console/index.ts
+export * from "./communication";
+export * from "./consoleConnection";
+export * from "./dolphinConnection";
+export * from "./types";
